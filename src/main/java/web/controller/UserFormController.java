95e32c77cec8db6332e616de1722fcda9c45432b
andreleao1 <andre.gustavo.ls@hotmail.com>
2021-03-15 11:11:18 -0300

    teste update

--- src/main/java/web/controller/UserFormController.java
+++ src/main/java/web/controller/UserFormController.java
@@ -14,6 +14,7 @@ import domain.model.TypePhone;
 import domain.model.User;
 import domain.service.IUserService;
 import domain.service.UserService;
+import jakarta.annotation.PostConstruct;
 
 @ManagedBean(name = "userFormController")
 @SessionScoped
@@ -27,14 +28,26 @@ public class UserFormController {
 	private IUserService userService;
 
 	public UserFormController() {
-		this.user = new User();
-		this.phone = new Phone();
-		this.phones = new HashSet<Phone>();
-		this.userService = new UserService();
+		if(this.user == null) {
+			this.user = new User();
+			this.phone = new Phone();
+			this.phones = new HashSet<Phone>();
+			this.userService = new UserService();
+			System.out.println("Chamou o construtor padrão.");
+			System.out.println(this.user.toString());
+		} else {
+
+			System.out.println("Pulou a verificação");
+			System.out.println(this.user.toString());
+		}
+		//System.out.println(this.user.toString());
+		
 	}
 
 	public UserFormController(User user) {
 		this.user = user;
+		System.out.println(this.user.toString());
+		System.out.println("Chamou o construtor com usuário");
 	}
 
 	public UserFormController(User user, Phone phone, String typePhoneChosen, Set<Phone> phones) {
@@ -42,6 +55,7 @@ public class UserFormController {
 		this.phone = phone;
 		this.phones = phones;
 		this.typePhoneChosen = typePhoneChosen;
+		System.out.println("Chamou o construtor com todos os atributos.");
 	}
 
 	public User getUser() {
@@ -80,7 +94,6 @@ public class UserFormController {
 		if (Objects.nonNull(this.phone)) {
 			assignPhoneToUser(this.phone);
 		}
-		System.out.println(this.user.toString());
 		this.userService.save(this.user);
 		clear();
 		loadPageUserList();
@@ -88,18 +101,19 @@ public class UserFormController {
 	
 	public void loadUserInForm(User user) {
 		try {
-			FacesContext.getCurrentInstance().getExternalContext().redirect("UserForm.xhtml");			
+			FacesContext.getCurrentInstance().getExternalContext().redirect("UserForm.xhtml");
+			System.out.println("Vai chamar o construtor agora");
 		} catch (Exception e) {
 			System.out.println(e.getStackTrace().toString());
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Erro ao carregar a página"));
-		}
-		this.user = user;
+		}		
 	}
 
 	public void loadPageUserList() {
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("UserList.xhtml");
+			//clear();
 		} catch (Exception e) {
 			System.out.println(e.getStackTrace().toString());
 			FacesContext.getCurrentInstance().addMessage(null,
--- src/main/java/web/controller/UserListController.java
+++ src/main/java/web/controller/UserListController.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import domain.model.User;
@@ -12,21 +13,22 @@ import domain.service.IUserService;
 import domain.service.UserService;
 
 @ManagedBean(name = "userListController")
-@ViewScoped
+@SessionScoped
 public class UserListController {
 	private User user;
 	private IUserService userService;
 	private List<User> listUsers;
-	User selectedUser;
+	private User selectedUser;
 
 	public UserListController() {
 		this.user = new User();
 		this.userService = new UserService();
 		this.listUsers = userService.findAll();
 		this.selectedUser = new User();
+		System.out.println("Chamou o construtor padrão");
 	}
 
-	public UserListController(User user, List<User> listUsers) {
+	public UserListController(User user, List<User> listUsers, String nameEmail) {
 		this.user = user;
 		this.listUsers = listUsers;
 		this.userService = new UserService();
@@ -43,12 +45,10 @@ public class UserListController {
 		this.reLoadPage();
 	}
 
-	public void loadUser() {
-		UserFormController userController = new UserFormController();
-		userController.loadUserInForm(this.selectedUser);
-	}
-
 	public void loadPageUserForm() {
+		if (this.selectedUser == null) {
+
+		}
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("UserForm.xhtml");
 		} catch (Exception e) {
@@ -57,7 +57,18 @@ public class UserListController {
 					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Erro ao carregar a página"));
 		}
 	}
-	
+
+	public void atualizar(User user) {
+		try {
+			this.user = user;
+			FacesContext.getCurrentInstance().getExternalContext().redirect("UserForm.xhtml");
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace().toString());
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Erro ao carregar a página"));
+		}
+	}
+
 	public void exit() {
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect("../index.xhtml");
